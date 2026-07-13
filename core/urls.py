3080df4bc4614207473d6ae780a807798f6e754a
Hilary Herrera <hilary.herrera@correounivalle.edu.co>
2026-07-12 19:12:16 -0500

    conexión de rutas proveedores, inventario y ventas en el core de la app

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from .views import inicio
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-
     path("", inicio),
-
     path("clientes/", include("apps.clientes.urls")),
-
+    path("proveedores/", include("apps.proveedores.urls")),
+    path("inventario/", include("apps.inventario.urls")),
+    path("ventas/", include("apps.ventas.urls")),
 ]
