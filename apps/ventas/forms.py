1950aa62d414da53e7298e6121045f292631bfd6
Hilary Herrera <hilary.herrera@correounivalle.edu.co>
2026-07-13 15:03:37 -0500

    no exigir total como campo lleno antes de pasar al siguiente y en su lugar colocar un mensaje no editable dentro de ese campo

--- apps/ventas/forms.py
+++ apps/ventas/forms.py
@@ -4,10 +4,10 @@ from .models import Orden
 class OrdenForm(forms.ModelForm):
     class Meta:
         model = Orden
-        fields = ["fecha", "total", "estado", "observaciones", "id_cliente", "id_empleado", "id_sede"]
+        fields = ["fecha", "estado", "observaciones", "id_cliente", "id_empleado", "id_sede"]
         widgets = {
             "fecha": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
-            "total": forms.NumberInput(attrs={"class": "form-control"}),
+            "total": forms.NumberInput(attrs={"class": "form-control", "readonly": "readonly", "placeholder": "Se calculará en automatico"}),
             "estado": forms.Select(attrs={"class": "form-select"}),
             "observaciones": forms.TextInput(attrs={"class": "form-control"}),
             "id_cliente": forms.Select(attrs={"class": "form-select"}),
